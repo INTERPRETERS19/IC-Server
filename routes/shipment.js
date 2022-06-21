26c86424ea9c43b4733128fd2c8643f460f6bf34
Gayathri-Sivakumar <79824505+Gayathri-Sivakumar@users.noreply.github.com>
2022-06-21 08:18:12 +0530

    shipment added

--- routes/shipment.js
+++ routes/shipment.js
@@ -1,8 +1,19 @@
 const express = require("express");
 
 const router = express.Router();
-const { createShipment } = require("../controllers/shipment");
-
+const {
+  createShipment,
+  getAllShipments,
+  getCollections,
+  getUsers,
+  getAllReturns,
+  getAllNewShipments,
+} = require("../controllers/shipment");
 router.post("/createsh", createShipment);
+router.get("/shipments", getAllShipments);
+router.get("/collections/:id", getCollections);
+router.get("/users", getUsers);
+router.get("/returns", getAllReturns);
+router.get("/newships", getAllNewShipments);
 
 module.exports = router;
